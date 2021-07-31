ea2b49499e0e5552ce1069991d86cfc919d065e0
Conrado Lanusse <conrado.lanusse@gmail.com>
2021-07-31 13:49:12 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hola profe sos el mejor
         </a>
       </header>
     </div>
