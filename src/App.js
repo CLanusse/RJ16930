56e0d145cf2cecc1e0f5e1ab86369baae997b6f9
Conrado Lanusse <conrado.lanusse@gmail.com>
2021-08-07 14:32:10 -0300

    Clase 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavBar } from "./components/NavBar/NavBar";
+import './styles/styles.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola profe sos el mejor
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <ItemListContainer greeting="Hola Coders!"/>
+
+    </>
+   
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './NavBar.scss'
+
+export const NavBar = () => {
+
+
+    return (
+        <header className="encabezado">
+            <h1>LOGO</h1>
+
+            <nav>
+                <a href="#">Enlace 1</a>
+                <a href="#">Enlace 2</a>
+                <a href="#">Enlace 3</a>
+            </nav>
+
+        </header>
+    )
+  
+}
